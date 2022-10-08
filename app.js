6ccce2cbf4e358200d9c2a5e7d60dc9c24d542fd
Bebop94 <jq.giannetti@gmail.com>
2022-10-08 18:06:26 -0300

    Heroku port

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log('Servidor funcionando');
 });
 
