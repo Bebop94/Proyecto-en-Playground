87b03af6ce7863404d6fd71addab4db5addbdd1b
Bebop94 <jq.giannetti@gmail.com>
2022-11-14 15:16:49 -0300

    JSON productos + EJS

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,13 +1,21 @@
 const fs = require("fs");
 const path = require("path");
 
-//const prodsFilePath = path.join(__dirname, "../data/products-data-base.json");
-//const products = JSON.parse(fs.readFileSync(prodsFilePath, 'utf-8'));
+const prodsFilePath = path.join(__dirname, "../data/product-inventory.json");
+const products = JSON.parse(fs.readFileSync(prodsFilePath, 'utf-8'));
 
 const mainController = {
 
     index: (req,res) => {
-        res.render('index')
+        let lastSearchedProducts = products.filter( product => {
+            return product.lastSearched == true
+        })
+
+        let discountProducts = products.filter( product => {
+            return product.oferta == true
+        })
+
+        res.render('index', {lastSearchedProducts, discountProducts})
     }
 }
 
